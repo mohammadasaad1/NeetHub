f029cf189c877593346f6d0729e9072c781a882f
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2024-12-12 14:03:51 +0200

    Time: 3 ms (54.53%), Space: 12.9 MB (89.48%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long pickGifts(vector<int> &gifts, int k) {
+        priority_queue<int> pq;
+        for(long long i : gifts)
+            pq.push(i) ;
+
+        while (k--) {
+            long long x = pq.top() ;
+            pq.pop() ;
+            x = sqrt(x) ;
+            pq.push(x) ;
+        }
+        long long ans = 0 ;
+
+        while (!pq.empty())
+            ans += pq.top() , pq.pop(); 
+        return ans ;
+    }
+};
