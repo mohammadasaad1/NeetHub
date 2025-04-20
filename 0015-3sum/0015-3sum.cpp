1f1ac44f4ecc351f8cc4abb321fdd8519b8a7489
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2025-04-20 16:25:43 +0300

    Time: 262 ms (19.29%), Space: 33.8 MB (23.16%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
+    struct obj {
+    int a , b , c;
+    bool operator<(const obj& other) const {
+        return tie(a, b, c) < tie(other.a, other.b, other.c);
+    }
+};
+    set<obj> st;
+
+void helper(int value , int startIdx , vector<int>& nums) {
+    int l = startIdx , r = nums.size() - 1 , target = 0;
+
+    while (l < r) {
+        if ((nums[l] + nums[r] + value) == target) {
+             st.insert({nums[l] , nums[r] , value });
+            l++; 
+        }
+        else if (nums[l] + nums[r] + value > target)
+            r--;
+        else l++ ;
+    }
+}
+
 vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = (long long)nums.size() ; 
-        sort(nums.begin() , nums.end()) ; 
-        set <tuple <int , int , int>> st ; 
-        for(int i = 0 ; i < n ; i++)
-        {
-            int l = i + 1 , r = n - 1 ;
-            
-            while (l < r)
-            {
-                int curr = nums[i] + nums[l] + nums[r] ;
-                if(curr == 0)
-                {
-                    st.insert({nums[i] , nums[l] , nums[r]}) ; 
-                    l++ , r-- ; 
-                }
-                else if(curr > 0)
-                {
-                    r-- ; 
-                }
-                else {
-                    l++ ; 
-                }
-            }
-             
-        }
-        vector<vector<int>> ret; 
-        for(auto i : st)
-        {
-            vector <int> now = { get<0>(i) , get<1>(i) , get<2>(i)} ; 
-            ret.push_back(now) ; 
-        }
-        return ret ; 
+    // I'll sort them to speed up process
+    sort(nums.begin(), nums.end());
+    int n = (int)nums.size();
+    for (int i = 0; i < n - 2; i++) {
+        int kv = nums[i];
+
+        // kv + iv + jv = 0 ; (*)
+        // for now i have kv as a fixed value then i'll search for (*) using 2 pointers
+
+        helper(kv , i + 1 , nums) ;
+    }
+    vector<vector<int>> res;
+    for (auto &[a , b , c] : st) {
+        res.push_back({a , b , c});
+    }
+    
+    return res;
 }
 };
