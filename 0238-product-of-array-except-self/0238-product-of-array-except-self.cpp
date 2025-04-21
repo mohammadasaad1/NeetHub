8ea5d555aa4bdf4e3b395dda097d8f605b7e9d8b
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2025-04-21 16:06:21 +0300

    Time: 0 ms (100%), Space: 40.1 MB (94.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-    int products = 1 , cntZero = 0;
-    for(long long i : nums) products *= i , i == 0 ? cntZero++ : 0 ;
-
-    vector<int> ans ;
-
-    if(!products) // there is one zero
-    {
-        products = 1 ;
-        for(long long j : nums)
-            if(j != 0)
-                products *= j ;
+        int n = nums.size(), pre = 1, post = 1;
+        vector<int> res(n, 1);
+        for (int i = 0; i < n; i++) {
+            res[i] = pre;
+            pre *= nums[i];
+        }
+        for (int i = n - 1; i > 0; i--) {
+            res[i] *= post;
+            post *= nums[i];
+        }
+        res[0] = post;
+        return res;
     }
-    for(long long j : nums)
-        ans.push_back(cntZero == 1 && j == 0 ? products : cntZero >= 1 ? 0 : products / j) ;
-
-    return ans ;
-}
 };
