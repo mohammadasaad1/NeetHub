5ad1e3f4ed1d32d8025efd4ce2710cb586e07bc2
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2025-04-22 14:04:58 +0300

    Time: 3 ms (41.8%), Space: 41.5 MB (96.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,32 @@
+import java.util.Stack;
 class Solution {
- 
-
+    private boolean isClosing(Character ch){
+       return (ch == ')' || ch == '}' || ch == ']');
+    }
+    private Character pts(Character ch){
+        if(ch == ')')
+        return '(';
+        if(ch == '}')
+        return '{';
+        if(ch == ']')
+        return '[';
+        return null;
+    }
     public boolean isValid(String s) {
-       Stack <Character> stk = new Stack <Character>() ; 
-        int n = s.length() ; 
-        for(int i = 0 ; i < n ; i++){
-            if(s.charAt(i) == '(')
-                stk.push(')') ; 
-            
-           else if(s.charAt(i) == '[')
-                stk.push(']') ; 
-            
-           else if(s.charAt(i) == '{')
-                stk.push('}') ; 
-            
-            else if (stk.empty() || stk.pop() != s.charAt(i)){
-                return false ; 
+            Stack<Character>stk = new Stack<Character>();
+        int n = s.length();
+        for(int i = 0 ; i < n; i++)
+        {
+            if(!stk.empty() && isClosing(s.charAt(i))){
+
+                if(stk.peek() == pts(s.charAt(i)))
+                    stk.pop();
+                else return false;
+            }
+            else{
+                stk.push(s.charAt(i));
             }
         }
-        return stk.empty() ; 
+        return stk.empty();
     }
 }
