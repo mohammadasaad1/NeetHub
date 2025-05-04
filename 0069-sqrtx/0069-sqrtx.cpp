ea356ac2b26de42aaad856ce332ee4bc2e3dfe5c
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2025-05-04 13:42:17 +0300

    Time: 0 ms (100%), Space: 8.5 MB (85.71%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 1 || x == 0) return x ; 
+
+        int l = 1 , r = x ; 
+        long long  ans = 0 ; 
+         
+        while(l <= r){
+            long long m = l + (r - l) / 2 ;
+            long long now = (long long)m * m ; 
+
+            if (now == x) return m ; 
+
+            
+
+            else if(now > x)
+                r = m - 1; 
+            else if(now < x) l = m + 1 , ans = max(ans , m); 
+             
+        }
+        return ans ; 
+    }
+};
