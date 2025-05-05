6e2d1d7d5b73e4482717923b307c4d9307d7f2af
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2025-05-06 01:23:47 +0300

    Time: 118 ms (5.05%), Space: 132.5 MB (5.1%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,24 +1,35 @@
 class Solution {
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    int ans = 0;
+    map<pair<int, int>, bool> vis;
+    void dfs(vector<vector<int>>& grid, int i, int j) {
+        
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) {
+            ans++;
+            return;
+        }
+        if (vis[{i, j}])
+            return;
+
+        if(grid[i][j] == 1)
+            vis[{i , j}] = 1; 
+            
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+        
+
+    }
+
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-
-        int dx[4] = {1, -1, 0, 0};
-        int dy[4] = {0, 0, -1, 1};
-        //           r    l    d  u
-        int ans = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 1) {
-                    ans += 4;
-                    for (int k = 0; k < 4; k++) {
-                        int ii = i + dx[k], jj = j + dy[k];
-                        if (ii >= 0 && ii < grid.size() && jj >= 0 && jj < grid[0].size() &&
-                            grid[ii][jj] == 1)
-                            ans--;
-                    }
-                }
-            }
-        }
+        int rows = grid.size(), columns = grid[0].size();
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                if (grid[i][j] == 1)
+                    dfs(grid, i, j);
         return ans;
     }
 };
