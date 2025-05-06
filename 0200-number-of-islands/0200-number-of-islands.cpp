cdbaff95b285cc3be09b1256a463c0a914fbaa42
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2025-05-06 10:31:49 +0300

    Time: 25 ms (73.04%), Space: 16.2 MB (76.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
-    const int N = 301;
-    map <pair <int , int> , vector <pair<int , int>>> adj;
-    bool vis[302][302]{};
-
-    int dx[4] = {1 , -1 , 0 , 0};
-    int dy[4] = {0 , 0 , -1 , 1};
-
-    void dfs(vector<vector<char>>& grid , int i , int j){
-        vis[i][j] = 1 ; 
-        cout << i << ' ' << j << '\n' ; 
-        for(auto ch : adj[{i , j}]){
-
-            if(!vis[ch.first][ch.second])
-                dfs(grid , ch.first , ch.second);
-        }
+    void dfs(int i , int j , vector<vector<char>>& grid){
+        if(i < 0 || j < 0 || j >= grid[0].size() || i >= grid.size() || grid[i][j] == '0')
+        return ;
+        grid[i][j] = '0';
+        dfs(i - 1 , j , grid);
+        dfs(i + 1 , j , grid);
+        dfs(i , j - 1, grid);
+        dfs(i , j + 1, grid);
     }
     int numIslands(vector<vector<char>>& grid) {
+        int r = grid.size() , c = grid[0].size() ; 
         int ans = 0 ; 
-        int n = grid.size() , m = grid[0].size();
-        for(int i = 0 ; i < n ; i++){
-            for(int j  = 0 ; j < m ; j++){
-                if(grid[i][j] == '1')
-                for(int k = 0 ; k < 4 ; k++){
-                    int ii = i + dx[k] , jj = j + dy[k];
-                        if(ii >= 0 && ii < n && jj >= 0 && jj < m){
-                                adj[{i , j}].push_back({ii , jj});
-                                // adj[{ii , jj}].push_back({i , j});
-                        }
-                }
-            }
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j  = 0 ; j < m ; j++){
-                 if(!vis[i][j] && grid[i][j] == '1'){
-                    dfs(grid , i , j);
-                    cout << "\n________________________________\n"; 
+        for(int i = 0 ; i < r ; i++){
+            for(int j = 0 ; j < c ; j++){
+                if(grid[i][j] == '1'){
                     ans++ ; 
+                    dfs(i , j , grid);
                 }
             }
         }
-        return ans; 
+        return ans ; 
     }
-               
 };
