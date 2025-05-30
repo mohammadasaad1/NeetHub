6c09c79d7b97c7fe5c1b48e60dad2fe569b69922
Mohammad Asaad <mohammadfoqha2@hotmail.com>
2025-05-30 15:42:40 +0300

    Time: 2 ms (75.18%), Space: 10.3 MB (89.79%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    bool dp[301];
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        dp[s.size()] = true;
+        int n = s.size();
+        for(int i = n - 1; i >= 0; i--){
+            for(auto w : wordDict){
+                if(i + w.size() <= n && s.substr(i , w.size()) == w)
+                    dp[i] = dp[i + w.size()];
+                
+                if(dp[i]) break; 
+            }
+        }
+        return dp[0];
+    }
+};
